d11f81658e97046140b6bb4fad1f4114c9aaa623
jasonzz10 <40416063+jasonzz10@users.noreply.github.com>
2020-09-24 14:36:25 +0800

    Add files via upload

---
+++ mytest.py
@@ -0,0 +1,25 @@
+def triangles():
+    x = [1]
+
+    while True:
+
+        yield x
+
+        y = [0] + x + [0]
+
+        x = [y[i] + y[i+1] for i in range(len(y)-1)]
+
+
+n = 0
+results = []
+for t in triangles():
+    results.append(t)
+    n = n + 1
+    if n == 10:
+        break
+
+for t in results:
+    print(t)
+
+        
+        
